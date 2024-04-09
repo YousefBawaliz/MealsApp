f87321c98c63ed16671d171a4831fc3fccb64624
Bolz <yousef2k012@gmail.com>
2024-04-09 09:39:37 +0300

    resetting changes

--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -1,6 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/data/dummy_data.dart';
 
-final mealsProvider = Provider((ref) {
-  return dummyMeals;
+final MyMealsProvider = Provider((ref) {
+  return dummyMeals[1];
+  //this is new code
+  final hello = "string";
 });
+<<<<<<< Updated upstream
+=======
+
+
+//this i
+//this is another commit
+>>>>>>> Stashed changes
